209ba7f29416eea9dce1cc81db7431ff2a5487dc
Rui Silva <rui.parakedista@gmail.com>
2023-09-07 20:32:40 +0100

    cv first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let exp1 = document.getElementById("exp1");
+exp1.addEventListener('click', function(){
+    let desc1 = document.getElementById("desc1");
+    // desc1.setAttribute('style', 'display:none');
+    desc1.classList.toggle('esconder');
+    desc1.classList.toggle('xpto');
+});
